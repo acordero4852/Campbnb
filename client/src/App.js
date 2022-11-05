0200539fd7831a4a318ca90beeae598d5a5abef6
acordero4852 <corderoandy5@gmail.com>
2022-11-05 16:39:24 -0400

    add some small feature to login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import Landing from './pages/Landing'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" exact element={<Landing/>}/>
+        <Route path="/" exact element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </div>
   );
