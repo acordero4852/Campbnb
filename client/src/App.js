8103ecfe8f7d105f92d44946bbf05ed86b2aa5da
acordero4852 <corderoandy5@gmail.com>
2022-11-05 13:08:38 -0400

    installed tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>Test test 1</h1>
+      <h1 class="text-3xl font-bold underline">
+        Hello World
+      </h1>
     </div>
   );
 }
