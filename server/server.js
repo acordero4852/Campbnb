fc1c61a32714a1e4a6cda1d6a13dc77069a20352
Paul Franco <paulfranco.e@gmail.com>
2022-11-05 13:51:18 -0400

    User auth and prisma db stuff

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var cors = require('cors');
+var app = express();
+var auth = require('./routes/auth')
+var users = require('./routes/user')
+
+app.listen(3001, function () {
+    console.log(`\uD83D\uDE80 Server ready at http://localhost:3001`);
+});
+
+app.use(express.json());
+app.use(cors());
+app.use('/auth', auth);
+app.use('/user', users)
+
+app.get('/', function (req, res) {
+    res.status(200).send({ server_is: "up" });
+});
